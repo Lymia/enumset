92b4df5914bf951b6d744f7d5d23c733de7fa693
Lymia Aluysia <lymia@lymiahugs.com>
2020-04-01 11:23:32 -0700

    Fix version check.

--- enumset/tests/trybuild.rs
+++ enumset/tests/trybuild.rs
@@ -1,6 +1,6 @@
 #[test]
 fn ui() {
-    if version_check::is_min_version("1.36.0") {
+    if version_check::is_min_version("1.36.0").expect("Could not get rustc version.") {
         let t = trybuild::TestCases::new();
         t.compile_fail("tests/compile-fail/*.rs");
     }
