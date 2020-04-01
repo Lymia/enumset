3423850652bd1e0a8dca547cd4d8e6dc56f69d08
Lymia Aluysia <lymia@lymiahugs.com>
2020-04-01 11:34:04 -0700

    Run ui tests only on nightly to avoid subtle format differences.

--- enumset/tests/trybuild.rs
+++ enumset/tests/trybuild.rs
@@ -1,7 +1,6 @@
+#[rustversion::nightly]
 #[test]
 fn ui() {
-    if version_check::is_min_version("1.36.0").expect("Could not get rustc version.") {
-        let t = trybuild::TestCases::new();
-        t.compile_fail("tests/compile-fail/*.rs");
-    }
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/compile-fail/*.rs");
 }
