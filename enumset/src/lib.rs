a655adc28579c04b989748f352b401d1b4999835
Alissa Rao <lymia@lymiahugs.com>
2023-05-01 05:49:14 -0700

    Update Rust version to 2021.

--- enumset/src/lib.rs
+++ enumset/src/lib.rs
@@ -823,7 +823,7 @@ impl<T: EnumSetType + Debug> Debug for EnumSet<T> {
     }
 }
 
-#[allow(clippy::derive_hash_xor_eq)] // This impl exists to change trait bounds only.
+#[allow(clippy::derived_hash_with_manual_eq)] // This impl exists to change trait bounds only.
 impl<T: EnumSetType> Hash for EnumSet<T> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.__priv_repr.hash(state)
